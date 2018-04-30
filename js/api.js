ca613a464e9d7a62b75409c54f66ecc8cbbad88b
Pavel Keyzik <pavel.keyzik@gmail.com>
2018-04-30 21:13:30 +0300

    Added render authors and pictures by author

--- js/api.js
+++ js/api.js
@@ -2,6 +2,18 @@ class Api {
   getYears() {
     return fetch("/data/years.json");
   }
+
+  getAuthors() {
+    return fetch("/data/authors.json");
+  }
+
+  /**
+   *
+   * @param {Number} id Id of author
+   */
+  getPicturesByAuthor(id) {
+    return fetch("/data/authors-id.json");
+  }
 }
 
 let api = new Api();
--- js/render.js
+++ js/render.js
@@ -29,6 +29,64 @@ class Render {
   clearBlock(block) {
     block.innerHTML = "";
   }
+
+  /**
+   *
+   * @param {Array} listOfAuthors
+   * @param {Number} currentPage
+   * @param {Number} countOfPages
+   */
+  renderAuthors(listOfAuthors, currentPage, countOfPages) {
+    this.clearBlock(this.root);
+
+    let compiled = _.template(`
+      <div class="authors">
+        <% _.forEach(listOfAuthors, (item) => { %>
+          <a href="#" class="authors__item">
+            <div class="authors__image">
+              <img src="<%= item.image %>" alt="">
+            </div>
+            <div class="authors__info">
+              <div class="authors__name"><%= item.name %></div>
+              <div class="authors__count-of-pictures"><%= item.count_of_pictures %> картин</div>
+            </div>
+          </a>
+        <% }) %>
+      </div>
+      <%= currentPage %>
+    `);
+
+    this.root.innerHTML = compiled({
+      listOfAuthors,
+      currentPage,
+      countOfPages
+    });
+  }
+
+  renderByAuthor(listOfPictures, authorInfo) {
+    this.clearBlock(this.root);
+
+    let compiled = _.template(`
+      <h2><%= authorInfo.name %></h2>
+      <img src="<%= authorInfo.image %>" alt="">
+      <p><%= authorInfo.description %></p>
+      <a class="gallery">
+        <% _.forEach(listOfPictures, (item) => { %>
+          <div class="gallery__item">
+            <div class="gallery__image">
+              <img src="<%= item.image %>" alt="">
+            </div>
+            <div class="gallery__info">
+              <div class="gallery__title"><%= item.name %></div>
+            </div>
+          </div>
+        <% }) %>
+      </a>
+    `);
+    // <%= item.name %>
+
+    this.root.innerHTML = compiled({ listOfPictures, authorInfo });
+  }
 }
 
 let render = new Render();
