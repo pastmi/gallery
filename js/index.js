d1d59fd0a5247180624f889949d371c7218fc69d
Гинько Михаил <accer96226@gmail.com>
2018-05-06 12:37:35 +0300

    add routing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
 import Main from './main';
+import {Router} from './router'
+
+var router = new Router({
+  routes: [
+    {
+      name: "index",
+      match: "",
+      onBeforeEnter: () => (window.location.hash = "exhibitions=1")
+    },
+    {
+      name: "exhibitions",
+      match: /exhibitions=(.+)/,
+      onEnter: page => console.log(`onEnter exhibitions:${page}`),
+      onLeave: page => console.log(`onLeave exhibitions:${page}`)
+    },
+    {
+      name: "author",
+      match: /author=(.+)/,
+      onEnter: page => console.log(`onEnter author:${page}`),
+      onLeave: page => console.log(`onLeave author:${page}`)
+    },
+    {
+      name: "year",
+      match: /year=(.+)/,
+      onEnter: page => console.log(`onEnter year:${page}`),
+      onLeave: page => console.log(`onLeave year:${page}`)
+    }
+  ]
+});
 
 let main = new Main();
